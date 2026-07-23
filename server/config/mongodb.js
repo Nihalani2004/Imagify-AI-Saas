a661fa4d7931836dc5ecb8abb8839bb2a3264b21
Mayank Nihalani <mayanknihalani24@gmail.com>
2026-07-23 17:46:23 +0530

    fix: use dbName option in mongoose.connect to prevent write concern URI conflict

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
-  mongoose.connection.on('connected',()=>{
+const connectDB = async () => {
+  mongoose.connection.on('connected', () => {
     console.log("Database connected")
   })
-  await mongoose.connect(`${process.env.MONGODB_URI}/imagify`)
+  await mongoose.connect(process.env.MONGODB_URI, { dbName: 'imagify' })
 }
 
 export default connectDB;
